0101804dbb4e406b7d95e610b39b52b8109d7ecf
sadgh77 <sadeghhadipour1@gmail.com>
2022-11-19 18:55:31 +0330

    add HomeBanner component

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="HomeBanner" class="w-100 bg-Banner">
+    <div class="py-3">
+      <h1 class="logo">Brand</h1>
+      <p class="description">A place to share your knowledge</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeBanner"
+}
+</script>
+
+<style scoped>
+.bg-Banner {
+  background-color: #5CB85C;
+}
+
+.logo {
+  color: white;
+  font-size: 80px;
+}
+
+.description {
+  color: white;
+  font-size: 20px;
+}
+</style>
